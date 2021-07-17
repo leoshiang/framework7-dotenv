7de1d105001f3346d0b63064d4a9cfe5bf32e94b
向皓田 <leoshiang@gmail.com>
2021-07-17 16:32:51 +0800

    顯示 .env 內容。

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,7 +9,7 @@
       <f7-nav-right>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
-      <f7-nav-title-large>Framework7 DotEnv Sample</f7-nav-title-large>
+      <f7-nav-title-large>{{apiUrl}}</f7-nav-title-large>
     </f7-navbar>
     <!-- Toolbar-->
     <f7-toolbar bottom>
@@ -66,3 +66,15 @@
     </f7-list>
   </f7-page>
 </template>
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+  data() {
+      return {
+      apiUrl: process.env.API_URL
+    }
+  }
+}
+</script>
